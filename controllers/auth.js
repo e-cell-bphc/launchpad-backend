7c0072d3601d5174f149433af1efbaa351dcc74b
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-01-25 13:12:10 +0530

    Implement login and signup

---
+++ controllers/auth.js
@@ -0,0 +1,80 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { TOKEN_SECRET } = require('../config')
+const { unexpectedError } = require('../errors/common')
+const { noUser, userCreationFailed } = require('../errors/auth')
+
+async function register(req, res) {
+  console.log(req.body)
+  const { name, email, password, college, phoneNumber } = req.body
+
+  let hashedPassword = await bcrypt.hash(password, 10)
+
+  try {
+    const result = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      college,
+      phoneNumber
+    })
+
+    console.log(result)
+
+    const token = jwt.sign(
+      {
+        _id: result._id,
+        email: result.email,
+        accessScopes: result.accessScopes
+      },
+      TOKEN_SECRET
+    )
+
+    return res.status(200).json({
+      status: 'ok',
+      desc: 'Account created',
+      data: { _id: result._id, token }
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(userCreationFailed)
+  }
+}
+
+async function login(req, res) {
+  console.log(req.body)
+  const { email, password } = req.body
+
+  const result = await User.findOne({ email }).lean()
+
+  if (!result) {
+    return res.status(400).json(noUser)
+  }
+
+  if (await bcrypt.compare(password, result.password)) {
+    console.log(result)
+
+    const token = jwt.sign(
+      {
+        _id: result._id,
+        email: result.email,
+        accessScopes: result.accessScopes
+      },
+      TOKEN_SECRET
+    )
+
+    return res.status(200).json({
+      status: 'ok',
+      desc: 'Account created',
+      data: { _id: result._id, token }
+    })
+  }
+
+  res.status(400).json(unexpectedError)
+}
+
+module.exports = {
+  register,
+  login
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,7 @@
 const router = require('express').Router()
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const { register, login } = require('../controllers/auth')
 
-router.post('/register', async (req, res) => {
-  console.log(req.body)
-  const { name, email, password, college, phoneNumber } = req.body
+router.post('/register', register)
+router.post('/login', login)
 
-  let hashedPassword = await bcrypt.hash(password, 10)
-
-  try {
-    const result = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-      college,
-      phoneNumber
-    })
-
-    console.log(result)
-    return res
-      .status(200)
-      .json({
-        status: 'ok',
-        desc: 'Account created',
-        data: { _id: result._id }
-      })
-  } catch (error) {
-    console.log(error)
-    return res
-      .status(500)
-      .json({ status: 'failed', desc: 'something went wrong' })
-  }
-})
 module.exports = router
