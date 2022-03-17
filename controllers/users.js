9f17baf4eb6019445258cd917d77dada675fd732
Sanchit Kalra <sanchit.kalra1@gmail.com>
2022-03-17 17:49:18 +0530

    Add file upload

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,21 @@
 var _ = require('lodash')
+const firebaseAdmin = require('firebase-admin')
+const multer = require('multer')
+
+const serviceAccount = require('./../service-account.json')
+const admin = firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount)
+})
+
+const storageRef = admin.storage().bucket(`gs://e-cell-bphc-22.appspot.com`)
+
+// Initiating a memory storage engine to store files as Buffer objects
+const uploader = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // limiting files size to 5 MB
+  }
+})
 
 const { noUser } = require('../errors/auth')
 var User = require('../models/User')
@@ -69,8 +86,104 @@ async function updateProfile(req, res) {
   })
 }
 
+// Upload endpoint to send file to Firebase storage bucket
+function uploadResume(req, res) {
+  console.log(req.files[0])
+  try {
+    if (!req.files[0]) {
+      // res.status(400).send('Error, could not upload file')
+      return res.status(400).json({
+        status: 'failed',
+        desc: 'Could not upload file'
+      })
+    }
+
+    const blob = storageRef.file(req.files[0].originalname)
+
+    const blobWriter = blob.createWriteStream({
+      metadata: {
+        contentType: req.files[0].mimetype
+      }
+    })
+
+    blobWriter.on('error', (err) => {
+      console.log(err)
+      return res.status(400).json({
+        status: 'failed',
+        desc: 'Could not upload file'
+      })
+    })
+
+    blobWriter.on('finish', () => {
+      async function saveURL() {
+        const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          storageRef.name
+        }/o/${encodeURI(blob.name)}?alt=media`
+        console.log(publicUrl)
+
+        const result = await User.updateOne(
+          { email: req.usr.email },
+          { $set: { resumeURL: publicUrl } }
+        )
+
+        if (!result) {
+          return res.status(400).json({
+            status: 'failed',
+            desc: 'Upload failed'
+          })
+        }
+
+        res.status(200).json({
+          status: 'ok',
+          desc: 'file uploaded successfully'
+        })
+      }
+
+      saveURL()
+    })
+
+    blobWriter.end(req.files[0].buffer)
+
+    // // Create new blob in the bucket referencing the file
+    // const blob = bucket.file(req.file.originalname)
+
+    // // Create writable stream and specifying file mimetype
+    // const blobWriter = blob.createWriteStream({
+    //   metadata: {
+    //     contentType: req.file.mimetype
+    //   }
+    // })
+
+    // blobWriter.on('error', (err) => next(err))
+
+    // blobWriter.on('finish', () => {
+    //   // Assembling public URL for accessing the file via HTTP
+    //   const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+    //     bucket.name
+    //   }/o/${encodeURI(blob.name)}?alt=media`
+
+    //   // Return the file name and its public URL
+    //   res
+    //     .status(200)
+    //     .send({ fileName: req.file.originalname, fileLocation: publicUrl })
+    // })
+
+    // When there is no more data to be consumed from the stream
+    // blobWriter.end(req.file.buffer)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      status: 'failed',
+      desc: 'Could not upload file'
+    })
+    // res.status(400).send(`Error, could not upload file: ${error}`)
+    // return
+  }
+}
+
 module.exports = {
   getProfile,
   getProfileWithAdminOrEditorScope,
-  updateProfile
+  updateProfile,
+  uploadResume
 }
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 const router = require('express').Router()
+const multer = require('multer')
+
 const {
   getProfile,
   getProfileWithAdminOrEditorScope,
-  updateProfile
+  updateProfile,
+  uploadResume
 } = require('../controllers/users')
+
 const {
   ensureAuthenticated,
   ensureAuthenticatedWithAdminEditorScope
 } = require('../middlewares/ensureAuthenticated')
+
+// Initiating a memory storage engine to store files as Buffer objects
+const uploader = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // limiting files size to 5 MB
+  }
+})
 
 router.get('/:id', ensureAuthenticated, getProfile)
 router.get(
@@ -17,5 +29,7 @@ router.get(
 )
 
 router.post('/updateProfile', updateProfile)
+
+router.post('/uploadResume', ensureAuthenticated, uploader.any(), uploadResume)
 
 module.exports = router
